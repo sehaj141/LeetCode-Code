3f2108d19664e84a736ef37e56114468c4fa3936
sehaj141 <14ksehaj@gmail.com>
2026-08-06 20:00:54 +0530

    Time: 0 ms (100%), Space: 9.9 MB (43.63%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string countAndSay(int n) {
         string result = "1";
-        for(int i=1; i<n; i++){
+        for(int i=1 ; i<n; i++){
             string current = "";
             int count = 1;
             for(int j=1; j<result.size(); j++){
