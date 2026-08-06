876b8e0cc595b61ad43fd9a38ee0fd9dc9a5b5ce
sehaj141 <14ksehaj@gmail.com>
2026-08-06 23:18:23 +0530

    Time: 7 ms (42.53%), Space: 9.8 MB (66.03%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -4,11 +4,28 @@ public:
         string repeated = a;
         int count = 1;
 
+        while(repeated.size() < b.size()){
+            repeated += a;
+            count++;
+        }
+        if(repeated.find(b) != string::npos){
+            return count;
+        }
+        repeated += a;
+        if(repeated.find(b) != string::npos){
+            return count + 1;
+        }
+        return -1;
+        /*
+        string repeated = a;
+        int count = 1;
+
         while (repeated.size() < b.size()) {
             repeated += a;
             count++;
         }
-
+        //string.find(substring)
+       // It searches for substring inside the string ; It returns: Index of the first occurrence if found ; string::npos if not found.
         if (repeated.find(b) != string::npos)
             return count;
 
@@ -18,5 +35,6 @@ public:
             return count + 1;
 
         return -1;
+        */
     }
 };
