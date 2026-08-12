b2a8fc893164e46b64f12dd31f3704101ff8f5a6
sehaj141 <14ksehaj@gmail.com>
2026-08-13 00:09:46 +0530

    Time: 122 ms (36.41%), Space: 26.2 MB (22.98%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int beautySum(string s) {
         int sum = 0;
-        for(int i=0 ;i<s.size(); i++){
+        for(int i=0; i<s.size(); i++){
             unordered_map<char,int> freq;
-            for(int j=i; j<s.size();j++){
+            for(int j=i; j<s.size(); j++){
                 freq[s[j]]++;
+
                 int maxi = INT_MIN;
                 int mini = INT_MAX;
 
                 for(auto it : freq){
-                    maxi = max(maxi,it.second);
+                    maxi = max(maxi, it.second);
                     mini = min(mini, it.second);
-
                 }
                 sum += (maxi-mini);
             }
-        
         }
         return sum;
     }
