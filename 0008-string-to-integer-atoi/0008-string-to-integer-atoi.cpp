a302920adbb845f30b7300050625ac6a5ff0a7c9
sehaj141 <14ksehaj@gmail.com>
2026-08-18 17:45:37 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.18%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-const int INT_MINI_VAL = -2147483648 ;
-const int INT_MAXI_VAL =  2147483647;
+    int INT_MIN_VAL = -pow(2,31) ; 
+    int INT_MAX_VAL = pow(2,31) - 1;
     int helper(string &s, int i, long long num, int sign){
         if(i >= s.size() || !isdigit(s[i])){
-            return (int)(num*sign);
+            return (int)(sign * num);
         }
         num = num*10 + (s[i] - '0');
 
-        if(sign*num <= INT_MINI_VAL) return INT_MINI_VAL;
-        if(sign*num >= INT_MAXI_VAL) return INT_MAXI_VAL;
+        if(sign*num <=  INT_MIN_VAL) return INT_MIN_VAL;
+        if(sign*num >= INT_MAX_VAL) return INT_MAX_VAL;
 
         return helper(s,i+1,num,sign);
     }
     int myAtoi(string s) {
-       int i = 0;
-       while(i< s.size() && s[i] == ' '){
-         i++;
-       } 
-       int sign = 1;
-       if(i<s.size() && (s[i] == '+' || s[i] == '-')){
-        sign = (s[i] == '-') ? -1 : 1;
-        i++;
-       }
-       return helper(s,i,0,sign);
+        int i=0;
+        while(i <s.size() && s[i] == ' '){
+            i++;
+        }
+        int sign = 1;
+        if(i < s.size() && (s[i] == '+' || s[i] == '-')){
+            sign = (s[i] == '-')?-1 : 1;
+            i++;
+        }
+        return helper(s,i,0,sign);
     }
 };
