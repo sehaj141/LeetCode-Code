de7572e7bd1bac6334c04367bacaa4aa40c075a9
sehaj141 <14ksehaj@gmail.com>
2026-08-19 13:11:39 +0530

    Time: 0 ms (100%), Space: 12 MB (38.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()) return "";
+        if(strs.empty()){
+            return "";
+        }
         sort(strs.begin(), strs.end());
 
         string first = strs[0];
@@ -9,7 +11,7 @@ public:
         string ans = "";
         int minLen = min(first.size(), last.size());
 
-        for(int i=0; i<minLen; i++){
+        for(int i=0; i< minLen; i++){
             if(first[i] != last[i]){
                 break;
             }
