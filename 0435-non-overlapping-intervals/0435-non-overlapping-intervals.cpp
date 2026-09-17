63c5b8e3628a05869f5fd88b08a6f3619ce8f71d
sehaj141 <14ksehaj@gmail.com>
2026-09-17 15:57:18 +0530

    Time: 43 ms (65.5%), Space: 94.1 MB (38.43%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,16 +4,16 @@ public:
         sort(intervals.begin(), intervals.end(), [](auto &a, auto &b){
             return a[1] < b[1];
         });
-        int count =0;
+        int count = 0;
         int prevEnd = intervals[0][1];
 
-        for(int i=1; i<intervals.size(); i++){
+        for(int i=1; i<intervals.size() ; i++){
             if(intervals[i][0] < prevEnd){
                 count++;
             }else{
                 prevEnd = intervals[i][1];
             }
-    }
+        }
         return count;
     }
 };
