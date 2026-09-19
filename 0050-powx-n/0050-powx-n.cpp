f8ff20ac971a0bd61508b0c582263795f8ad342a
sehaj141 <14ksehaj@gmail.com>
2026-09-19 23:13:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Class definition for the solution
 class Solution {
+private:
+    // Function to calculate power
+    // of 'x' raised to 'n'
+    double power(double x, long n) {
+        // Base case: anything raised to 0 is 1
+        if (n == 0) return 1.0;
+
+        // Base case: anything raised to 1 is itself
+        if (n == 1) return x;
+
+        // If 'n' is even
+        if (n % 2 == 0) {
+            // Recursive call: x * x, n / 2
+            return power(x * x, n / 2);
+        }
+        // If 'n' is odd
+        // Recursive call: x * power(x, n-1)
+        return x * power(x, n - 1);
+    }
+
 public:
+    // Function to calculate x raised to n
     double myPow(double x, int n) {
-        return pow(x,n);
+        // Store the value of n in a separate variable
+        long num = n;
+
+        // If n is negative
+        if (num < 0) {
+            // Calculate the power of -n and take reciprocal
+            return (1.0 / power(x, -1 * num));
+        }
+        // If n is non-negative
+        return power(x, num);
     }
 };
+
